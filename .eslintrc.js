26c88d1c36b2673f172d45c0d9687d8c3eab82a8
Stephan Bakker <info@stephanbakker.nl>
2016-06-27 21:29:52 +0200

    add .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "Google",
+    "rules": {
+        "indent": ["error", 4],
+        "require-jsdoc": 0
+    },
+    "plugins": [
+        "react"
+    ],
+    "env": {
+        "shared-node-browser": true
+    }
+};
