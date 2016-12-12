3d3a1d49c4ba0240977f1a189c5992f8fdf9db88
Stephan Bakker <info@stephanbakker.nl>
2016-12-12 21:58:17 +0100

    fix tags again

--- shared/modules/Photos.js
+++ shared/modules/Photos.js
@@ -5,140 +5,150 @@ import Tags from './Tags';
 import GalleryButtons from './GalleryButtons';
 
 class Photos extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            activeIndex: -1,
-            tagIndex: 0,
-            currentTag: Tags.getTags(this.props.photos).shift()
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: -1,
+      tagIndex: 0,
+      currentTag: Tags.getTags(this.props.photos).shift()
+    };
 
-        this.toggle = this.toggle.bind(this);
-        this.collapse = this.collapse.bind(this);
-        // this.expand = this.expand.bind(this);
-        this.next = this.next.bind(this);
-        this.previous = this.previous.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.collapse = this.collapse.bind(this);
+    this.updateTag = this.updateTag.bind(this);
+    // this.expand = this.expand.bind(this);
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("keyup", this.handleKeyUp.bind(this));
+  }
+
+  componentWillUnMount() {
+    window.removeEventListener("keyup", this.handleKeyUp.bind(this));
+  }
+
+  render() {
+    let photos = this.state.currentTag ?
+      this.props.photos.filter(photo =>
+        this.filterTaggedPhotos(photo, this.state.currentTag)) :
+      this.props.photos;
+
+    let thumbs = photos.map((photo, index) => {
+      const key = 'p' + index;
+      return (
+        <li key={key} className="overview__item">
+          <Photo
+            onClick={this.toggle(index)}
+            data={photo}
+            ref={'photo' + index} />
+        </li>
+      );
+    });
+
+    const showButtons = this.state.activeIndex !== -1;
+    return (
+      <div>
+        <Tags
+          photos={this.props.photos}
+          update={this.updateTag}
+          current={this.state.currentTag} />
+        <ul className="overview">
+          {thumbs}
+        </ul>
+        <GalleryButtons
+          show={showButtons}
+          collapse={this.collapse}
+          next={this.next}
+          previous={this.previous} />
+      </div>
+    );
+  }
+
+  toggle(index) {
+    return function togglePhoto(evt) {
+      evt.preventDefault();
+      if (this.state.activeIndex === index) {
+        this.collapse({ transition: 'zoom' });
+      } else {
+        this.expand(index, { transition: 'zoom' });
+      }
+    }.bind(this);
+  }
+
+  collapse(config) {
+    const photo = this.refs['photo' + this.state.activeIndex];
+    if (photo) {
+      photo.setState({
+        isActive: false,
+        transition: config.transition
+      });
+    }
+    this.setState({
+      activeIndex: -1
+    });
+  }
+
+  expand(index, config) {
+    const photo = this.refs['photo' + index];
+    if (photo) {
+      photo.setState({
+        isActive: true,
+        transition: config.transition
+      });
+    }
+    this.setState({
+      activeIndex: index
+    });
+  }
+
+  next() {
+    let next = this.state.activeIndex + 1;
+    if (this.props.photos.length === next) {
+      next = -1;
+    }
+    this.collapse({ transition: 'opacity' });
+    this.expand(next, { transition: 'opacity' });
+  }
+
+  previous() {
+    const previous = this.state.activeIndex - 1;
+    this.collapse({ transition: 'opacity' });
+    this.expand(previous, { transition: 'opacity' });
+  }
+
+  handleKeyUp(evt) {
+    if (this.state.activeIndex < 0) {
+      return;
     }
 
-    componentDidMount() {
-        window.addEventListener("keyup", this.handleKeyUp.bind(this));
+    switch (evt.which) {
+      case 27: // Escape
+        this.collapse({transition: 'zoom'});
+        break;
+      case 39: // ArrowRight
+        this.next();
+        break;
+      case 37: // ArrowLeft
+        this.previous();
+        break;
+      default:
     }
+  }
 
-    componentWillUnMount() {
-        window.removeEventListener("keyup", this.handleKeyUp.bind(this));
-    }
+  updateTag(tag) {
+    this.setState({
+      currentTag: this.state.currentTag !== tag ? tag : null
+    });
+  }
 
-    render() {
-        let photos = this.state.currentTag ?
-                                this.props.photos
-                                    .filter(photo => this.filterTaggedPhotos(photo, this.state.currentTag)) :
-                                this.props.photos;
+  filterTaggedPhotos(photo, tag) {
+    return Tags
+      .getTagsFromPhoto(photo.tags)
+      .indexOf(tag) > -1;
+  }
 
-        let thumbs = photos.map((photo, index) => {
-            const key = 'p' + index;
-            return (
-                    <li key={key} className="overview__item">
-                        <Photo
-                            onClick={this.toggle(index)}
-                            data={photo}
-                            ref={'photo' + index}/>
-                    </li>
-                );
-        }
-        );
-
-        const showButtons = this.state.activeIndex !== -1;
-        return (
-            <div>
-                <Tags photos={this.props.photos} update={this.updateTag} current={this.state.currentTag}/>
-                <ul className="overview">
-                    {thumbs}
-                </ul>
-                <GalleryButtons
-                    show={showButtons}
-                    collapse={this.collapse}
-                    next={this.next}
-                    previous={this.previous} />
-            </div>
-        );
-    }
-
-    toggle(index) {
-        return function togglePhoto(evt) {
-            evt.preventDefault();
-            if (this.state.activeIndex === index) {
-                this.collapse({transition: 'zoom'});
-            } else {
-                this.expand(index, {transition: 'zoom'});
-            }
-        }.bind(this);
-    }
-
-    collapse(config) {
-        const photo = this.refs['photo' + this.state.activeIndex];
-        if (photo) {
-            photo.setState({
-                isActive: false,
-                transition: config.transition
-            });
-        }
-        this.setState({
-            activeIndex: -1
-        });
-    }
-
-    expand(index, config) {
-        const photo = this.refs['photo' + index];
-        if (photo) {
-            photo.setState({
-                isActive: true,
-                transition: config.transition
-            });
-        }
-        this.setState({
-            activeIndex: index
-        });
-    }
-
-    next() {
-        let next = this.state.activeIndex + 1;
-        if (this.props.photos.length === next) {
-            next = -1;
-        }
-        this.collapse({transition: 'opacity'});
-        this.expand(next, {transition: 'opacity'});
-    }
-
-    previous() {
-        const previous = this.state.activeIndex - 1;
-        this.collapse({transition: 'opacity'});
-        this.expand(previous, {transition: 'opacity'});
-    }
-
-    handleKeyUp(evt) {
-        if (this.state.activeIndex < 0) {
-            return;
-        }
-
-        switch (evt.which) {
-        case 27: // Escape
-            this.collapse({transition: 'zoom'});
-            break;
-        case 39: // ArrowRight
-            this.next();
-            break;
-        case 37: // ArrowLeft
-            this.previous();
-            break;
-        default:
-        }
-    }
-
-    updateTag(tag) {
-        this.setState({
-            currentTag: this.state.currentTag !== tag ? tag : null
-        });
-    }
 }
 
 export default Photos;
--- shared/modules/Tags.js
+++ shared/modules/Tags.js
@@ -4,51 +4,58 @@ import React from 'react';
 import Tag from './Tag';
 
 class Tags extends React.Component {
-    constructor(props) {
-        super(props);
-        this.setFilter = this.setFilter.bind(this);
-    }
-    static getTags(photos) {
-        return getUniqueTags(photos);
-    }
-    static getTagsFromPhoto(tags) {
-        return tags
-            .split(' ')
-            .filter(tag => tag !== '');
-    }
-    render() {
-        let tags = Tags.getTags(this.props.photos).map((tag, index) => {
-            let isCurrent = tag === this.props.current;
-            return (
-                            <li className="tags__item" key={'tag' + index}>
-                                <Tag
-                                    onClick={this.setFilter(tag)}
-                                    tag={tag}
-                                    active={isCurrent}/>
-                            </li>
-                        );
-        });
-        return (
-           <ul className="tags">
-                {tags}
-            </ul>
-        );
-    }
-    setFilter(tag) {
-        return (() => this.props.update(tag));
-    }
+  constructor(props) {
+    super(props);
+    this.setFilter = this.setFilter.bind(this);
+  }
+  static getTags(photos) {
+    return getUniqueTags(photos);
+  }
+  static getTagsFromPhoto(tags) {
+    return tags
+      .split(' ')
+      .filter(tag => tag !== '');
+  }
+  render() {
+    let tags = Tags.getTags(this.props.photos).map((tag, index) => {
+      let isCurrent = tag === this.props.current;
+      console.log('inTags iscurrent: ', isCurrent, tag);
+      console.log('inTags props: ', this.props.current);
+      return (
+        <li className="tags__item"
+          key={'tag' + index}
+          onClick={() => this.setFilter(tag)}>
+          <Tag
+            tag={tag}
+            active={isCurrent} />
+        </li>
+      );
+    });
+    return (
+      <ul className="tags">
+        {tags}
+      </ul>
+    );
+  }
+  setFilter(tag) {
+    console.log('jooo')
+    console.log('joooo clicked');
+    const isActive = this.props.current === tag;
+    console.log(tag, this.props.current, 'and???')
+    this.props.update(isActive ? '' : tag);
+  }
 }
 
 export default Tags;
 
 function getUniqueTags(photos) {
-    return photos.reduce(reduceTags, []).filter(uniq);
+  return photos.reduce(reduceTags, []).filter(uniq);
 }
 
 function reduceTags(acumulator, photo) {
-    return acumulator.concat(Tags.getTagsFromPhoto(photo.tags));
+  return acumulator.concat(Tags.getTagsFromPhoto(photo.tags));
 }
 
 function uniq(item, index, array) {
-    return array.indexOf(item) === index;
+  return array.indexOf(item) === index;
 }
