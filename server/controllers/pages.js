4a303484466ac0396e689a5d17d6f272756bee5a
Stephan Bakker <info@stephanbakker.nl>
2016-12-13 11:37:01 +0100

    linting

--- server/controllers/pages.js
+++ server/controllers/pages.js
@@ -6,21 +6,21 @@ import getContentPages from './update-content-pages';
 export default updatePages;
 
 function updatePages(req, res, next) {
-    return getContentPages()
-        .then(pages => datastore.updatePages(pages, 'content'))
-        .then(data => {
-            const updated = data.content.map(page => page.title).join(',');
-            if (res) {
-                res.end('Pages updated on request: %s', updated);
-            } else {
-                console.log('Pages updated: %s', updated);
-            }
-        })
-        .catch(err => {
-            if (res) {
-                res.sendStatus(500, err);
-            }
-            console.log('Error updating pages (500)', err);
-        });
+  return getContentPages()
+    .then(pages => datastore.updatePages(pages, 'content'))
+    .then(data => {
+      const updated = data.content.map(page => page.title).join(',');
+      if (res) {
+        res.end('Pages updated on request: %s', updated);
+      } else {
+        console.log('Pages updated: %s', updated);
+      }
+    })
+    .catch(err => {
+      if (res) {
+        res.sendStatus(500, err);
+      }
+      console.log('Error updating pages (500)', err);
+    });
 }
 
