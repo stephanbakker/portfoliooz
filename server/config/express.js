dc186d492133c9cf156ec85a11fc648f8c68201e
Stephan Bakker <info@stephanbakker.nl>
2016-12-13 14:44:03 +0100

    linting

--- server/config/express.js
+++ server/config/express.js
@@ -5,8 +5,8 @@ const favicon = require('serve-favicon');
 import errorMiddleware from '../middlewares/error-middleware';
 
 module.exports = function(app) {
-    app.use(express.static(path.join(__dirname, '../../public')));
-    app.use(favicon(path.join(__dirname, '../../public/favicon.ico')));
-    app.use(errorMiddleware);
+  app.use(express.static(path.join(__dirname, '../../public')));
+  app.use(favicon(path.join(__dirname, '../../public/favicon.ico')));
+  app.use(errorMiddleware);
 };
 
