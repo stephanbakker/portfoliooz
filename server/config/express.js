4f048c6ef8ccea19e0d6df1650315b73b3a626da
Stephan Bakker <info@stephanbakker.nl>
2017-03-03 19:46:55 +0100

    add compression and helmet

--- server/config/express.js
+++ server/config/express.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
 const path = require('path');
 const favicon = require('serve-favicon');
 
 import errorMiddleware from '../middlewares/error-middleware';
 
 module.exports = function(app) {
+  app.use(compression());
+  app.use(helmet());
   app.use(express.static(path.join(__dirname, '../../public')));
   app.use(favicon(path.join(__dirname, '../../public/favicon.ico')));
   app.use(errorMiddleware);
